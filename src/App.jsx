3a99009234d5ba44a4ffe77902e7a1ab5a5d2fa1
Crisnzano <crispus.nzano@gmail.com>
2024-01-29 10:35:38 +0300

    Create App.jsx
    feat(core): add App.jsx
    
    Created the App.jsx file that acts as the SPA for all the components and imported them

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import About from "./componets/About";
+import Home from "./componets/Home";
+import Contact from "./componets/Contact";
+import Experience from "./componets/Experience"
+import Footer from "./componets/Footer";
+import Header from "./componets/Header";
+import Portfolio from "./componets/Portfolio";
+import Service from "./componets/Service";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <About/>
+      <Service/>
+      <Experience/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+
+
+
+     
+    </div>
+  );
+}
