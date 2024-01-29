4399f305555720770c5eb08b7aa1718740ab128f
Crisnzano <crispus.nzano@gmail.com>
2024-01-29 10:58:24 +0300

    Create index.js
    fix(upgrade) add images index.js
    
    Fixed the problem of images not loading by adding a index.js file under images to store the images in a constant called IMAGES then exported it using 'export default'

---
+++ src/images/index.js
@@ -0,0 +1,10 @@
+const IMAGES = {
+    imageOne: require('./profile-pic.JPG'),
+    imageTwo: require('./about-pic.JPG'),
+    imageThree: require('./project-1.png'),
+    imageFour: require('./project-2.png'),
+    imageFive: require('./project-3.png'),
+    imageSix: require('./project-4.png'),
+    imageSeven: require('./project-6.png')
+}
+export default IMAGES;
