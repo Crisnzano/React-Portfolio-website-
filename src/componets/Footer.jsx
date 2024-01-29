514ac0d92c8fa2fdf06454685be6d13092f8b7e3
Crisnzano <crispus.nzano@gmail.com>
2024-01-29 10:50:41 +0300

    Create Footer.jsx
    feat(core): add footer
    
    Added the Footer component under the src folder and exported it using 'export default function'

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="footer"> Created by <span>Crispus Nzano</span> | All rights Reserved
+
+        </footer>
+
+    );
+}
