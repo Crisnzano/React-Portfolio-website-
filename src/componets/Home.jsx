76a1451da2016c1823b0b1f9c8448041b19fe636
Crisnzano <crispus.nzano@gmail.com>
2024-01-29 10:54:33 +0300

    Create Home.jsx
    feat(core): add home
    
    Added the Home component under the src folder and exported it using 'export default function'

---
+++ src/componets/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import IMAGES from "../images/index.js";
+export default function Home() {
+    return (
+        <div>
+      <section className="home" id="home">
+        <div className="content">
+          <span className="hi"> Hello...</span>
+          <h3> My name is <span> Crispus Nzano</span></h3>
+          <p className="info">I am a front-end developer</p>
+          <p className="text">I'm a web designer, graphics designer and Frontend developer. I studied for a Bachelor's Degree in Business Information and Technology.
+          Welcome to my Portfolio website.</p>
+          <a href="#about" className="btn">About me</a>
+      </div>
+    <img className="image" src={IMAGES.imageOne }alt=""
+    />
+
+      </section>
+    </div>
+
+    );
+}
